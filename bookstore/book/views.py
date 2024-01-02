b03060d76f6d148596874e4eb4b7a81d80d26371
kilamaelie <elijahkilama14@gmail.com>
2024-01-02 22:31:26 +0300

    django-tutorial

---
+++ bookstore/book/views.py
@@ -0,0 +1,8 @@
+# Create your views here.
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
