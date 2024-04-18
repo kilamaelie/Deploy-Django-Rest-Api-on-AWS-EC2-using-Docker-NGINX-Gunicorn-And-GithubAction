21bd7982fae0bbda11ffd0f9c723b45184701366
kilama <elijahkilama14@gmail.com>
2024-04-18 18:31:24 +0300

    deploy to aws

--- store/core/urls.py
+++ store/core/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.contrib import admin # type: ignore
+from django.urls import path, include # type: ignore
+from rest_framework.routers import DefaultRouter # type: ignore
 from book.views import BookViewSet
-from django.conf.urls.static import static
-from django.conf import settings
+from django.conf.urls.static import static # type: ignore
+from django.conf import settings# type: ignore
 
 router = DefaultRouter()
 router.register(r'book', BookViewSet)
